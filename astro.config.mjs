0aefe9138df7737894d9381e02d649dc19d60c8b
kur0sawa <167412275+kur0sawa@users.noreply.github.com>
2026-08-04 13:11:42 +0700

    feat: add XML sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://fahrizalyakin.pages.dev",
-  
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
